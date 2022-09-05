fe9ba9e36f49dae2cd5aa7e0a9cb5d8e19ce1c06
ggenilson <genilsonaraujo20002002@gmail.com>
2022-09-05 02:03:36 +0100

    feat: add routes and page configuration

---
+++ src/pages/Main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainPage: React.FC = () => {
+  return <h1>Main Page</h1>;
+};
+
+export default MainPage;
---
+++ src/pages/Parts.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PartsPage: React.FC = () => {
+  return <h1>Parts Page</h1>;
+};
+
+export default PartsPage;
